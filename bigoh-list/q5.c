9acecbe309673f0d6484e227ea1c2ab43e3c2f86
davi-canuto <davicanutogregorio@gmail.com>
2022-06-04 12:51:23 -0300

    brute force list

---
+++ bigoh-list/q5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int menor_elemento(){
+
+}
+
+int main(){
+  int n, x, ind;
+  printf("Digite o tamanho do array e o valor de X ");
+  scanf("%d %d",&n, &x);
+  int arr[n];
+
+  for(ind=0 ; ind<n ; ind++){
+  }
+}
---
+++ brute-force-list/mmc_question.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int n1, n2, resultado_imc, resultado;
+  printf("Digite os dois números que queres o MMC");
+  scanf ("%d %d",&n1,&n2);
+
+  while(n1 != 1 && n2 != 1){
+    
+  }
+}
